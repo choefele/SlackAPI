bb885405c1854e664ee68bd3711118164ffb00b6
Claus Höfele <claus@claushoefele.com>
2016-06-19 21:39:07 +0200

    Implement listFiles()

--- Sources/SlackClient.swift
+++ Sources/SlackClient.swift
@@ -10,18 +10,37 @@ import Foundation
 
 private let SLACK_API = "https://slack.com/api"
 
-class SlackClient {
+public class SlackClient {
     private let session = URLSession(configuration: URLSessionConfiguration.default())
     private let token: String
     
-    init(token: String) {
+    public init(token: String) {
         self.token = token
     }
     
-    func listFiles(to: Date = Date()) {
+    public func listFiles(to: Date = Date(), completionHandler: ([File]?) -> Void) {
         let types = "all"
         let count = 5
-
-        let _ = URL(string: SLACK_API + "/files.list?token=\(token)&types=\(types)&count=\(count)&ts_to=\(to)")!
+        let apiURL = URL(string: SLACK_API + "/files.list?token=\(token)&types=\(types)&count=\(count)&ts_to=\(to.timeIntervalSince1970)")!
+        let dataTask = session.dataTask(with: apiURL) { (data, response, error) in
+            if let _ = error {
+                completionHandler(nil)
+                return
+            }
+            
+            guard let data = data,
+                let jsonObject = try? JSONSerialization.jsonObject(with: data),
+                let jsonDictionary = jsonObject as? [String: AnyObject],
+                let jsonFiles = jsonDictionary["files"] as? [[String: AnyObject]],
+                let files = Parser.parseFiles(JSON: jsonFiles),
+                let jsonPage = jsonDictionary["paging"] as? [String: AnyObject],
+                let _ = Parser.parsePage(JSON: jsonPage) else {
+                completionHandler(nil)
+                return
+            }
+            
+            completionHandler(files)
+        }
+        dataTask.resume()
     }
 }
